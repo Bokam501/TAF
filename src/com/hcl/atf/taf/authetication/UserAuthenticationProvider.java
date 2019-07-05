e49437d6c962e4e41f54439109b74a1ebb9f2695
Bokam501 <44878100+Bokam501@users.noreply.github.com>
2019-07-05 15:36:20 +0530

    Add files via upload

---
+++ src/com/hcl/atf/taf/authetication/UserAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.hcl.atf.taf.authetication;
+import java.util.ArrayList;
+
+import java.util.List;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+public class UserAuthenticationProvider implements AuthenticationProvider {
+
+	private static final List<GrantedAuthority> AUTHORITIES
+		= new ArrayList<GrantedAuthority>();
+
+	static {
+		AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_ADMMIN"));
+		AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_TESTER"));
+		AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+		
+	}
+
+	@Override
+	public Authentication authenticate(Authentication auth)
+			throws AuthenticationException {
+
+		if (auth.getName().equals(auth.getCredentials())) {
+			return new UsernamePasswordAuthenticationToken(auth.getName(),
+				auth.getCredentials(), AUTHORITIES);
+		}
+
+		throw new BadCredentialsException("Bad Credentials");
+
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+
+		if ( authentication == null ) return false;
+		return Authentication.class.isAssignableFrom(authentication);
+
+	}
+  
+}
